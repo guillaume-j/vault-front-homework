294d930b80d241eec6a833ea21268deba395a628
guillaume <guillaume.jeanne@epitech.eu>
2023-09-10 17:47:07 +0200

    feat(basics): eslint added with basics rules | fixed code in app.tsx and prepared typing

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,60 @@ import TextInput from "./components/TextInput";
 
 const API = "http://localhost:5000";
 
-type Notif = {
+interface transactionNotificationData {
+  amout: number;
+  from: string;
+  id: number;
+  to: string;
+  unit: any;
+
+
+
+}
+
+interface accountNotificationData {
+  currency: string;
+  id: number;
+
+
+
+  name: string;
+}
+
+type NotifData = {
+  TRANSACTION_SEND: transactionNotificationData;
+  TRANSACTION_RECEIVED: transactionNotificationData;
+  ACCOUNT_CREATED: accountNotificationData;
+};
+
+type NotifType = 'TRANSACTION_SEND' | 'TRANSACTION_RECEIVED' | 'ACCOUNT_CREATED';
+
+type Notif<T extends NotifType> = {
   id: string;
-  type: string;
-  // FIXME we should *probably* not have this `any`
-  data: any;
+  type: T;
+  data: NotifData[T];
 };
 
 const App = () => {
   const [searchText, setSearchText] = useState("");
   const [isLoading, setLoading] = useState(false);
-  const [results, setResults] = useState<null | Notif[]>(null);
+  const [results, setResults] = useState<null | (Notif<'TRANSACTION_SEND' | 'TRANSACTION_RECEIVED'> | Notif<'ACCOUNT_CREATED'>)[]>(null);
 
   useEffect(() => {
     const effect = async () => {
-      // FIXME there is something wrong with this loading state... to be investigated :D
       setLoading(true);
-      const res = await fetch(`${API}/search?q=${searchText}`);
-      const data = await res.json();
-      setResults(data);
+
+
+
+
+                try {
+        const res = await fetch(`${API}/search?q=${searchText}`);
+        const data = await res.json();
+        setResults(data);
+      } catch (e) {
+        console.log(e)
+      }
+      setLoading(false);
     };
     effect();
   }, [searchText, setLoading, setResults]);
@@ -33,10 +68,22 @@ const App = () => {
         <div>{"Loading..."}</div>
       ) : results ? (
         <div>
-          {results.map((r) => (
-            // TODO we must finalize this integration!! not very pretty like this
+          {results.map((r) => {
+            if (r.type === 'ACCOUNT_CREATED') {
+              const data = r.data as accountNotificationData;
+              return (
+              <div className="border flex flex-row place-content-around">
+                <p>{data.name}</p>
+                <p>{data.currency}</p>
+                <p>{data.id}</p>
+              </div>
+              )
+            }
+            // const data = r.data as transactionNotificationData;
+             return (
             <div className="border border-dashed">{JSON.stringify(r)}</div>
-          ))}
+            )
+          })}
         </div>
       ) : null}
     </div>
