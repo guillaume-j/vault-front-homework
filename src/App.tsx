31588459cb9594a975dd70720733e3ee999e6f38
guillaume <guillaume.jeanne@epitech.eu>
2023-09-11 12:10:02 +0200

    feat(eslint & back): added more config to eslint and new fix for backend to allow user to not enter event type in capital letters

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
-import { useEffect, useState } from "react";
-import TextInput from "./components/TextInput";
+import { useEffect, useState } from 'react';
+import TextInput from './components/TextInput';
 
-const API = "http://localhost:5000";
+const API = 'http://localhost:5000';
 
-interface transactionNotificationData {
+interface TransactionNotificationData {
   amout: number;
   from: string;
   id: number;
   to: string;
-  unit: any;
-
-
-
+  unit: string;
 }
 
-interface accountNotificationData {
+interface AccountNotificationData {
   currency: string;
   id: number;
-
-
-
   name: string;
 }
 
 type NotifData = {
-  TRANSACTION_SEND: transactionNotificationData;
-  TRANSACTION_RECEIVED: transactionNotificationData;
-  ACCOUNT_CREATED: accountNotificationData;
+  TRANSACTION_SEND: TransactionNotificationData;
+  TRANSACTION_RECEIVED: TransactionNotificationData;
+  ACCOUNT_CREATED: AccountNotificationData;
 };
 
 type NotifType = 'TRANSACTION_SEND' | 'TRANSACTION_RECEIVED' | 'ACCOUNT_CREATED';
@@ -37,24 +31,24 @@ type Notif<T extends NotifType> = {
   data: NotifData[T];
 };
 
-const App = () => {
-  const [searchText, setSearchText] = useState("");
+type PossibleNotifType = Notif<'TRANSACTION_SEND' | 'TRANSACTION_RECEIVED'> | Notif<'ACCOUNT_CREATED'>;
+
+function App() {
+  const [searchText, setSearchText] = useState('');
   const [isLoading, setLoading] = useState(false);
-  const [results, setResults] = useState<null | (Notif<'TRANSACTION_SEND' | 'TRANSACTION_RECEIVED'> | Notif<'ACCOUNT_CREATED'>)[]>(null);
+  // eslint-disable-next-line no-spaced-func
+  const [results, setResults] = useState<null | PossibleNotifType[]>(null);
 
   useEffect(() => {
     const effect = async () => {
       setLoading(true);
 
-
-
-
-                try {
+      try {
         const res = await fetch(`${API}/search?q=${searchText}`);
         const data = await res.json();
         setResults(data);
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
       setLoading(false);
     };
@@ -65,29 +59,34 @@ const App = () => {
     <div>
       <TextInput value={searchText} onChange={setSearchText} placeholder="Type to filter events" />
       {isLoading ? (
-        <div>{"Loading..."}</div>
-      ) : results ? (
+        <div>Loading...</div>
+      ) : (
         <div>
-          {results.map((r) => {
+          {results?.map((r) => {
             if (r.type === 'ACCOUNT_CREATED') {
-              const data = r.data as accountNotificationData;
+              const data = r.data as AccountNotificationData;
               return (
-              <div className="border flex flex-row place-content-around">
-                <p>{data.name}</p>
-                <p>{data.currency}</p>
-                <p>{data.id}</p>
-              </div>
-              )
+                <div className="border flex flex-row place-content-around" key={r.id}>
+                  <p>{data.name}</p>
+                  <p>{data.currency}</p>
+                  <p>{data.id}</p>
+                </div>
+              );
             }
-            // const data = r.data as transactionNotificationData;
-             return (
-            <div className="border border-dashed">{JSON.stringify(r)}</div>
-            )
+            const data = r.data as TransactionNotificationData;
+            return (
+              <div className="border flex flex-row place-content-around" key={r.id}>
+                <p>{data.amout}</p>
+                <p>{data.from}</p>
+                <p>{data.to}</p>
+                <p>{data.unit}</p>
+              </div>
+            );
           })}
         </div>
-      ) : null}
+      )}
     </div>
   );
-};
+}
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import eslintPlugin from 'vite-plugin-eslint'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
   plugins: [
     react(),
     eslintPlugin({
       cache: false,
-      include: ['./src/**/*.js', './src/**/*.jsx'],
+      include: ['./src/**/*.ts', './src/**/*.tsx'],
       exclude: [],
     }),
   ],
